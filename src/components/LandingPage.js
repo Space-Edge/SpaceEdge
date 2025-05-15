8f7c248d33d7fb7bbdaa2cbd685375c27d768359
Space-Edge <spacedgehosting@gmail.com>
2025-05-15 13:41:47 +0530

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -23,7 +23,7 @@ const LandingPage = () => {
                     rel="noopener noreferrer"
                     className="bg-black text-white px-4 py-2.5 rounded-lg font-semibold fixed top-1 right-6 z-50"
                 >
-                    Get In Touch
+                    Get a quote
                 </a>
             </nav>
 
